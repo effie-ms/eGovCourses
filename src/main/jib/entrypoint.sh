0adf519bf792b51c43a2ba93ce66e871fdbb552c
Effie <effie.mias@gmail.com>
2018-12-04 23:09:32 +0200

    Refactoring

--- src/main/jib/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "The application will start in ${SLEEP_TIME}s..." && sleep ${SLEEP_TIME}
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.egov.elearning.EgovcoursesApp"  "$@"
